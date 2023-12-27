5316b99e099b44e2f1ff1ab654783da102695a34
‎ <>
2023-12-27 11:46:18 +0200

    ip whitelist

--- src/main/java/org/hey/renegade/Renegade.java
+++ src/main/java/org/hey/renegade/Renegade.java
@@ -1,16 +1,30 @@
 package org.hey.renegade;
 
+import org.bukkit.BanList;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.profile.PlayerProfile;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -20,37 +34,33 @@ public final class Renegade extends JavaPlugin implements Listener {
     public static String version = "v1";
     public static String path = "plugins/renegade/";
     public static String name = "players.txt";
-    List<Renegade.Player> allowed_players = new ArrayList<Renegade.Player>();
 
-//    public void write(String stuff) {
-//        try {
-//            FileOutputStream fileOut = new FileOutputStream(Renegade.path);
-//            GZIPOutputStream gzOut = new GZIPOutputStream(fileOut);
-//            BukkitObjectOutputStream out = new BukkitObjectOutputStream(gzOut);
-//            out.write(stuff.getBytes());
-//            out.close();
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
     String get_path() {
         return Renegade.path + Renegade.name;
     }
 
     public void write(Renegade.Player player)  {
         try {
-            FileWriter file_writer = new FileWriter(get_path());
+            List<Renegade.Player> allowed_players = read();
+            for (int i = 0; i < allowed_players.size(); i++) {
+                Renegade.Player existing_player = allowed_players.get(i);
+                if (Objects.equals(existing_player.name, player.name) && !Objects.equals(player.ip, existing_player.ip)) {
+                    allowed_players.set(i, player);
+                }
+            }
+            StringBuilder output = new StringBuilder(Renegade.version + "\n");
+            output.append(player.name).append(":").append(player.ip).append("\n");
+
+            FileWriter file_writer = new FileWriter(get_path(), false);
             BufferedWriter writer = new BufferedWriter(file_writer);
-            writer.append(player.name+":"+player.ip+"\n");
+            writer.write(output.toString());
             writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-//        getConfig().set("players."+name, stuff);
-//        saveConfig();
     }
 
-    public void read() {
+    public List<Renegade.Player> read() throws IOException {
         try {
             FileReader file_reader = new FileReader(get_path());
             BufferedReader reader = new BufferedReader(file_reader);
@@ -59,12 +69,23 @@ public final class Renegade extends JavaPlugin implements Listener {
             String result;
             while ((result = reader.readLine()) != null) {
                 String[] player = result.split(":");
+                System.out.println("here: "+player[0]+player[1]);
                 players.add(new Renegade.Player(player[0], player[1]));
             }
-            this.allowed_players = players;
+            return players;
         } catch (IOException e) {
             System.out.println(e.getMessage());
+            throw e;
         }
+    }
+
+    public boolean exists(List<Renegade.Player> players, Renegade.Player new_player) {
+        for (Player player : players) {
+            if (Objects.equals(player.name, new_player.name) && Objects.equals(player.ip, new_player.ip)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void create_dirs() {
@@ -96,6 +117,8 @@ public final class Renegade extends JavaPlugin implements Listener {
         check_version();
 
         getServer().getPluginManager().registerEvents(this, this);
+        getCommand("add_ip").setExecutor(new Renegade.CommandAdd());
+
     }
 
     @Override
@@ -103,52 +126,85 @@ public final class Renegade extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        System.out.println("OwO");
-    }
 
-    @EventHandler
-    public void onPlayerChat(AsyncPlayerChatEvent event) {
-        event.setCancelled(true);
-        System.out.println("canceled dude2");
-    }
-
-    @EventHandler
-    public void onHarvestBlockEvent(PlayerHarvestBlockEvent event) {
-        event.setCancelled(true);
-        System.out.println("canceled dude3");
-    }
-
-    @EventHandler
-    public void onPlayerMoveEvent(PlayerMoveEvent event) {
-        //event.setCancelled(true);
-        //System.out.println("canceled dude4");
-    }
-
-    @EventHandler
-    public void onPlayerWorldEvent(PlayerChangedMainHandEvent event) {
-
-        System.out.println("world boom");
-    }
-
-    @EventHandler
-    public void onPlayerBreak(PlayerItemBreakEvent event) {
-
-        System.out.println("breakkkkkkkkkkkkk");
-    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerDamage(PlayerItemDamageEvent event) {
-        event.setCancelled(true);
-        System.out.println("dmageeeeeeeeeee");
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        Server server = getServer();
+        ConsoleCommandSender console = server.getConsoleSender();
+        org.bukkit.entity.Player player = event.getPlayer();
+        String name = player.getName();
+        InetAddress inet = event.getAddress();
+        String ip = inet.getHostAddress();
+        Renegade.Player renegete_player = new Renegade.Player(name, ip);
+        try {
+            String reason = "Ask Hey.";
+            if (name.contains(":") || name.contains("\n")){
+                reason = "Invalid name.";
+                console.sendMessage("Banned for invalid name: "+ip);
+            }
+            else if (!exists(read(), renegete_player)) {
+                reason = "Ask Hey.";
+                console.sendMessage("Banned not whitelisted ip: "+ip);
+            }
+            else {
+                boolean unbanned = false;
+
+                if (server.getIPBans().contains(ip)) {
+                    server.unbanIP(inet);
+                    unbanned = true;
+                }
+
+                BanList<PlayerProfile> banList = server.getBanList(BanList.Type.PROFILE);
+                PlayerProfile player_profile = player.getPlayerProfile();
+                if (banList.isBanned(player_profile)) {
+                    banList.pardon(player_profile);
+                    unbanned = true;
+                }
+
+                if (unbanned) {
+                    console.sendMessage("Unbanned: "+ip);
+                    event.setResult(PlayerLoginEvent.Result.ALLOWED);
+                }
+
+                return;
+            }
+
+            server.banIP(inet);
+            event.setKickMessage(reason);
+            event.setResult(PlayerLoginEvent.Result.KICK_WHITELIST);
+            System.out.println(name + ":" + ip);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            player.kickPlayer("Error");
+            server.banIP(inet);
+        }
     }
 
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        event.setCancelled(true);
-       // System.out.println("break block oof " + this.test);
-       // this.test++;
+    public class CommandAdd implements CommandExecutor {
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            try {
+                if (args.length != 2) return false;
+                String name = args[0];
+                String ip = args[1];
+                InetAddress inet = InetAddress.getByName(ip);
+                Renegade.Player player = new Renegade.Player(name, ip);
+
+                Server server = getServer();
+                ConsoleCommandSender console = server.getConsoleSender();
+
+                server.unbanIP(inet);
+                write(player);
+
+                console.sendMessage("Added: "+name+":"+ip);
+                return true;
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                return false;
+            }
+        }
+
     }
 
     public class Player {
@@ -157,7 +213,7 @@ public final class Renegade extends JavaPlugin implements Listener {
 
         public Player(String name, String ip) {
             this.name = name;
-            this.ip = name;
+            this.ip = ip;
         }
     }
 }
